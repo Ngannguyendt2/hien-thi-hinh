576baed0218d4134374dcf678388c2c17f1b0e10
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-09-06 11:43:55 +0700

    create file show image

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="rectangle.php">
+    <input type="submit" value="Rectangle">
+</form>
+<form action="squaretriangle.php">
+    <input type="submit" value="SquareTriangle">
+</form>
+<form action="isoscelesTriangle.php">
+    <input type="submit" value="isoscelesTriangle">
+</form>
+</body>
+</html>
